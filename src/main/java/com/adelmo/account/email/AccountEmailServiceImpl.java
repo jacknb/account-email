6d67cf1b88f45444733760e824df196dc42d7baf
jacknb <zhuningbin@sina.com>
2017-05-25 22:05:08 +0800

    Init

--- src/main/java/com/adelmo/account/email/AccountEmailServiceImpl.java
+++ src/main/java/com/adelmo/account/email/AccountEmailServiceImpl.java
@@ -1,12 +1,48 @@
 package com.adelmo.account.email;
 
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 /**
  * Created by znb on 17-5-15.
  */
 public class AccountEmailServiceImpl implements AccountEmailService {
 
+    private JavaMailSender javaMailSender;
+    private String systemEmail;
+
     @Override
     public void sendMail(String to, String subject, String text) throws AccountEmailException {
+        try {
+            MimeMessage mimeMailMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMailMessage);
+            mimeMessageHelper.setFrom(systemEmail);
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(text, true);
 
+            javaMailSender.send(mimeMailMessage);
+        } catch (MessagingException e) {
+            throw new AccountEmailException("Faild to send mail.", e);
+        }
+    }
+
+    public JavaMailSender getJavaMailSender() {
+        return javaMailSender;
+    }
+
+    public void setJavaMailSender(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public String getSystemEmail() {
+        return systemEmail;
+    }
+
+    public void setSystemEmail(String systemEmail) {
+        this.systemEmail = systemEmail;
     }
 }
